23e260de5686f985ab9febec689d94eb57ea4adb
jeankarlo_santana@bhd.com.do <jeankarlo_santana@bhd.com.do>
2025-07-06 00:40:14 -0400

    created app entities without any relationship

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using TaskManagement.Infrastructure.DataAccess;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<TaskManagementContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options => 
+    {
+        options.SwaggerEndpoint("/opeapi/v1.json", "Demo Api");
+    });
 }
 
 app.UseHttpsRedirection();
